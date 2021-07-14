9e8ea1a159f9efbdb1597b99e41196188b9c712e
Ali Shehbaz <alishahbaz072@gmail.com>
2021-07-13 21:42:07 -0400

    particle config change

--- src/config/particle-config.js
+++ src/config/particle-config.js
@@ -1,32 +1,140 @@
 const ParticlesConfig = {
+    // particles: {
+    //   number: {
+    //     value: 71,
+    //     density: {
+    //       enable: true,
+    //       value_area: 946.9771699587272
+    //     }
+    //   },
+    //   color: {
+    //     value: "#3be816"
+    //   },
+    //   shape: {
+    //     type: "triangle",
+    //     stroke: {
+    //       width: 2,
+    //       color: "#ffffff"
+    //     },
+    //     polygon: {
+    //       nb_sides: 4
+    //     },
+    //     image: {
+    //       src: "img/github.svg",
+    //       width: 150,
+    //       height: 100
+    //     }
+    //   },
+    //   opacity: {
+    //     value: 0.08017060304327614,
+    //     random: false,
+    //     anim: {
+    //       enable: false,
+    //       speed: 1,
+    //       opacity_min: 0.1,
+    //       sync: false
+    //     }
+    //   },
+    //   size: {
+    //     value: 88.18766334760375,
+    //     random: true,
+    //     anim: {
+    //       enable: false,
+    //       speed: 40,
+    //       size_min: 0.1,
+    //       sync: false
+    //     }
+    //   },
+    //   line_linked: {
+    //     enable: true,
+    //     distance: 150,
+    //     color: "#ffffff",
+    //     opacity: 0.4088700755207083,
+    //     width: 1
+    //   },
+    //   move: {
+    //     enable: true,
+    //     speed: 6,
+    //     direction: "none",
+    //     random: false,
+    //     straight: false,
+    //     out_mode: "out",
+    //     bounce: false,
+    //     attract: {
+    //       enable: false,
+    //       rotateX: 600,
+    //       rotateY: 1200
+    //     }
+    //   }
+    // },
+    // interactivity: {
+    //   detect_on: "canvas",
+    //   events: {
+    //     onhover: {
+    //       enable: true,
+    //       mode: "repulse"
+    //     },
+    //     onclick: {
+    //       enable: true,
+    //       mode: "push"
+    //     },
+    //     resize: true
+    //   },
+    //   modes: {
+    //     grab: {
+    //       distance: 400,
+    //       line_linked: {
+    //         opacity: 1
+    //       }
+    //     },
+    //     bubble: {
+    //       distance: 400,
+    //       size: 40,
+    //       duration: 2,
+    //       opacity: 8,
+    //       speed: 3
+    //     },
+    //     repulse: {
+    //       distance: 200,
+    //       duration: 0.4
+    //     },
+    //     push: {
+    //       particles_nb: 4
+    //     },
+    //     remove: {
+    //       particles_nb: 2
+    //     }
+    //   }
+    // },
+    // retina_detect: true
     particles: {
       number: {
-        value: 71,
+        value: 80,
         density: {
           enable: true,
-          value_area: 946.9771699587272
+          value_area: 800
         }
       },
       color: {
-        value: "#3be816"
+        value: "#ffffff"
       },
       shape: {
-        type: "triangle",
+        type: "circle",
         stroke: {
-          width: 2,
-          color: "#ffffff"
+          width: 0,
+          color: "#000000"
         },
         polygon: {
-          nb_sides: 4
+          nb_sides: 5
         },
         image: {
           src: "img/github.svg",
-          width: 150,
+          width: 100,
           height: 100
         }
       },
       opacity: {
-        value: 0.08017060304327614,
+        value: 0.5,
         random: false,
         anim: {
           enable: false,
@@ -36,7 +144,7 @@ const ParticlesConfig = {
         }
       },
       size: {
-        value: 88.18766334760375,
+        value: 3,
         random: true,
         anim: {
           enable: false,
@@ -49,7 +157,7 @@ const ParticlesConfig = {
         enable: true,
         distance: 150,
         color: "#ffffff",
-        opacity: 0.4088700755207083,
+        opacity: 0.4,
         width: 1
       },
       move: {
@@ -107,6 +215,7 @@ const ParticlesConfig = {
       }
     },
     retina_detect: true
+    
   }
 
 export default ParticlesConfig;

32b52e83972495b7e6ef911a9fe710ab11067f5b
Ali Shehbaz <alishahbaz072@gmail.com>
2021-08-04 19:17:00 -0400

    gh pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from './LandingPage';
-import  Home from './Home';
-import  Education from './Education';
-import Projects from './Projects';
 
 function App() {
 
     return (
        
-       <Router>
-        <Route exact path="/">
-        <Home />
-        </Route>
-        <Route exact path="/education">
-        <Education />
-        </Route>
-        <Route exact path="/projects">
-        <Projects />
-        </Route>
-        <Route exact path="/tech-stack">
-        <TechStack />
-        </Route>
-        <Route exact path="/landing-page">
-        <LandingPage />
-        </Route>
-        </Router>
-  
+        <LandingPage/>
   
     );
   }
